9a738c5add4f1038551bf703a2de846377432ac5
MH <mathhan01@gmail.com>
2019-04-09 11:22:01 +0200

    added methods to receiver

---
+++ Lab_Assignment3_CommandPattern/Receivers/AmericanCook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab_Assignment3_CommandPattern.Receivers
+{
+    public class AmericanCook
+    {
+        #region Constructor
+
+        public AmericanCook()
+        {
+            
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void MakeShake()
+        {
+            Console.WriteLine("Blending ingredients...");
+            Console.WriteLine("Adding ice cream...");
+            Console.WriteLine("Mixing...");
+            Console.WriteLine("Finished shake!");
+        }
+
+        public void MakePizzaBurger()
+        {
+            Console.WriteLine("Toasting bread...");
+            Console.WriteLine("Adding cheese and salami...");
+            Console.WriteLine("Cook until melted...");
+            Console.WriteLine("Finished pizzaburger!");
+        }
+
+        public void MakeRibs()
+        {
+            Console.WriteLine("Clean ribs...");
+            Console.WriteLine("Marinate meat...");
+            Console.WriteLine("Grill meat...");
+            Console.WriteLine("Finished ribs!");
+        }
+
+        #endregion 
+    }
+
+
+}
